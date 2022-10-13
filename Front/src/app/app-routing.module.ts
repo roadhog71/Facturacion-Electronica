99501cb892d303814763c3bd9f84d940ae6bf2ed
Seb <63566437+roadhog71@users.noreply.github.com>
2022-10-13 15:08:11 -0300

    invoice-donwloader routed correctly

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-
+import { InvoiceDownloaderComponent } from './invoice-downloader/invoice-downloader.component';
 const routes: Routes = [
   {
     path: '',
@@ -13,6 +13,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'invoice-downloader',
+    component: InvoiceDownloaderComponent,
   },
 ];
 
