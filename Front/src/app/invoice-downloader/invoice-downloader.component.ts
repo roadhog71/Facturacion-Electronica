06fd99a87e1ceb29ea9edc61b44e446e3a32d328
Seb <63566437+roadhog71@users.noreply.github.com>
2022-10-14 17:01:28 -0300

    invoice-downloader component created and done 70%

--- Front/src/app/invoice-downloader/invoice-downloader.component.ts
+++ Front/src/app/invoice-downloader/invoice-downloader.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-invoice-downloader',
@@ -7,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoiceDownloaderComponent implements OnInit {
   selected = '';
+  hide: boolean = false;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onLogin() {
+    if (!this.loginForm.valid) {
+      return;
+    }
+    console.log(this.loginForm.value);
+  }
 }
