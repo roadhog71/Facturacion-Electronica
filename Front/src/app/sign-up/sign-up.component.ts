93232981003faec64d3d50af6587d37e9c08da18
Seb <63566437+roadhog71@users.noreply.github.com>
2022-10-21 15:59:46 -0300

    sign-up component 30% done

--- Front/src/app/sign-up/sign-up.component.ts
+++ Front/src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
+  hide: boolean = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  signupForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+  onLogin() {
+    if (!this.signupForm.valid) {
+      return;
+    }
+    console.log(this.signupForm.value);
   }
-
 }
